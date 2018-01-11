742485b51fb590684e92b0786acb50012da1229a
Tomoaki Nakajima <powered.by.solaris@gmail.com>
2018-01-11 15:00:09 +0900

    add url info

--- setup.sh
+++ setup.sh
@@ -18,3 +18,14 @@ docker run -d -p 8888:8888 --name jupyter \
            irixjp/aitac-automation:${aitac-2018-v01:?}
 
 docker exec jupyter git clone https://github.com/irixjp/aitac-automation-handson.git
+
+echo ''
+echo ''
+echo 'Access to the below url'
+echo '###############################################################'
+echo ''
+echo `docker logs jupyter 2>&1 | grep "all ip addresses on your system" | grep "/?token=" | tail -1`
+echo ''
+echo '###############################################################'
+echo ''
+echo ''
